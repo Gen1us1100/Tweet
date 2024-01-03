ef615685b0f6bae18a320adc373319809870682e
Sameer Godse <coder.sam.1100@gmail.com>
2024-01-04 01:00:31 +0530

    served the static files on fastAPI :(

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,7 +3,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope):
+        response = await super().get_response(path, scope)
+        if response.status_code == 404:
+            response = await super().get_response('.', scope)
+        return response
+
 
 app.add_middleware( 
     CORSMiddleware,
@@ -12,17 +21,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-model = pickle.load(open('app/tweetModel.sav', 'rb'))
-myVectorizer = joblib.load('app/myVectorizer.pkl')
+# model = pickle.load(open('app/tweetModel.sav', 'rb'))
+# myVectorizer = joblib.load('app/myVectorizer.pkl')
+
+model = pickle.load(open('tweetModel.sav', 'rb'))
+myVectorizer = joblib.load('myVectorizer.pkl')
 
 class Tweet(BaseModel):
     text: str
-    
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
 @app.put("/analysis/")
 def predict_analysis(tweet:Tweet):
@@ -30,3 +36,8 @@ def predict_analysis(tweet:Tweet):
     Tweet_vector =  myVectorizer.transform(myTweet)
     prediction = model.predict(Tweet_vector)
     return('looks positive to me! 😃' if prediction else 'looks negative to me!😕') 
+
+app.mount("/", StaticFiles(directory="staticfiles",html=True), name="static")
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
