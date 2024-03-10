94ca91d61ccd49e49662283c1b13e0a9e916de90
Sameer Godse <coder.sam.1100@gmail.com>
2024-03-10 18:44:48 +0530

    trying chatGPT for cloud

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,175 @@
+# import pickle
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import joblib
+# from starlette.middleware.cors import CORSMiddleware
+# from fastapi.staticfiles import StaticFiles
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
+# # from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
+# import time
+# import logging
+# # import chromedriver_binary
+# log_level = logging.DEBUG
+# logging.basicConfig(level=log_level)
+# webdriver_log = logging.getLogger('selenium.webdriver.remote.remote_connection')
+# webdriver_log.setLevel(log_level)
+
+# app = FastAPI()
+# class SPAStaticFiles(StaticFiles):
+#     async def get_response(self, path: str, scope):
+#         response = await super().get_response(path, scope)
+#         if response.status_code == 404:
+#             response = await super().get_response('.', scope)
+#         return response
+
+
+# app.add_middleware( 
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+# # model = pickle.load(open('app/tweetModel.sav', 'rb'))
+# # myVectorizer = joblib.load('app/myVectorizer.pkl')
+
+# model = pickle.load(open('tweetModel.sav', 'rb'))
+# myVectorizer = joblib.load('myVectorizer.pkl')
+
+# class Tweet(BaseModel):
+#     text: str
+
+# class Url(BaseModel):
+#     URL:str
+
+# @app.put("/analysis/")
+# def predict_analysis(tweet:Tweet):
+#     myTweet = {tweet.text}
+#     Tweet_vector =  myVectorizer.transform(myTweet)
+#     prediction = model.predict(Tweet_vector)
+#     return('looks positive to me! 😃' if prediction else 'looks negative to me!😕')
+
+# @app.put("/twitter/")
+# def tweet_analysis(url:Url):
+#     myURL = {url.URL}
+#     myTweet,UserName = getTweetTextandUsername(next(iter(myURL)))
+#     Tweet_vector =  myVectorizer.transform({myTweet})
+#     prediction = model.predict(Tweet_vector)
+#     return{'response':('looks positive to me! 😃' if prediction else 'looks negative to me!😕')
+#     ,'tweet_text':f'{myTweet}','username':f'{UserName}'}
+ 
+
+# app.mount("/", StaticFiles(directory="staticfiles",html=True), name="static")
+# @app.get("/")
+# def read_root():
+#     return {"Hello":"World"}
+
+# def getTweetTextandUsername(target_url:str):
+#     # print("URL IS THIS :"+target_url)
+#     # print("URL IS THIS :",str(type(target_url)))
+#     options = Options()
+#     options.add_argument('--headless')
+#     driver=webdriver.Firefox( options=options)
+#     driver.get(target_url)
+#     time.sleep(5)
+#     resp = driver.page_source
+#     soup=BeautifulSoup(resp,'html.parser')
+#     TweetText = soup.find("div",{"class":"css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-1inkyih r-16dba41 r-bnwqim r-135wba7"}).text
+#     TweetText = TweetText.replace('\n','')
+#     UserName = soup.find("div",{"class":"css-1rynq56 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-18u37iz r-1wvb978"}).text
+#     driver.close()
+#     return TweetText,UserName
+
+#Chat GPT experimentation
+
+# import pickle
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import joblib
+# from starlette.middleware.cors import CORSMiddleware
+# from fastapi.staticfiles import StaticFiles
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
+# # from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
+# import time
+# import logging
+# # import chromedriver_binary
+# log_level = logging.DEBUG
+# logging.basicConfig(level=log_level)
+# webdriver_log = logging.getLogger('selenium.webdriver.remote.remote_connection')
+# webdriver_log.setLevel(log_level)
+
+# app = FastAPI()
+# class SPAStaticFiles(StaticFiles):
+#     async def get_response(self, path: str, scope):
+#         response = await super().get_response(path, scope)
+#         if response.status_code == 404:
+#             response = await super().get_response('.', scope)
+#         return response
+
+
+# app.add_middleware( 
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+# # model = pickle.load(open('app/tweetModel.sav', 'rb'))
+# # myVectorizer = joblib.load('app/myVectorizer.pkl')
+
+# model = pickle.load(open('tweetModel.sav', 'rb'))
+# myVectorizer = joblib.load('myVectorizer.pkl')
+
+# class Tweet(BaseModel):
+#     text: str
+
+# class Url(BaseModel):
+#     URL:str
+
+# @app.put("/analysis/")
+# def predict_analysis(tweet:Tweet):
+#     myTweet = {tweet.text}
+#     Tweet_vector =  myVectorizer.transform(myTweet)
+#     prediction = model.predict(Tweet_vector)
+#     return('looks positive to me! 😃' if prediction else 'looks negative to me!😕')
+
+# @app.put("/twitter/")
+# def tweet_analysis(url:Url):
+#     myURL = {url.URL}
+#     myTweet,UserName = getTweetTextandUsername(next(iter(myURL)))
+#     Tweet_vector =  myVectorizer.transform({myTweet})
+#     prediction = model.predict(Tweet_vector)
+#     return{'response':('looks positive to me! 😃' if prediction else 'looks negative to me!😕')
+#     ,'tweet_text':f'{myTweet}','username':f'{UserName}'}
+ 
+
+# app.mount("/", StaticFiles(directory="staticfiles",html=True), name="static")
+# @app.get("/")
+# def read_root():
+#     return {"Hello":"World"}
+
+# def getTweetTextandUsername(target_url:str):
+#     # print("URL IS THIS :"+target_url)
+#     # print("URL IS THIS :",str(type(target_url)))
+#     options = Options()
+#     options.add_argument('--headless')
+#     driver=webdriver.Firefox( options=options)
+#     driver.get(target_url)
+#     time.sleep(5)
+#     resp = driver.page_source
+#     soup=BeautifulSoup(resp,'html.parser')
+#     TweetText = soup.find("div",{"class":"css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-1inkyih r-16dba41 r-bnwqim r-135wba7"}).text
+#     TweetText = TweetText.replace('\n','')
+#     UserName = soup.find("div",{"class":"css-1rynq56 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-18u37iz r-1wvb978"}).text
+#     driver.close()
+#     return TweetText,UserName
+
+#Chat GPT experimentation
+
 import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -24,7 +196,6 @@ class SPAStaticFiles(StaticFiles):
             response = await super().get_response('.', scope)
         return response
 
-
 app.add_middleware( 
     CORSMiddleware,
     allow_origins=["*"],
@@ -32,8 +203,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# model = pickle.load(open('app/tweetModel.sav', 'rb'))
-# myVectorizer = joblib.load('app/myVectorizer.pkl')
 
 model = pickle.load(open('tweetModel.sav', 'rb'))
 myVectorizer = joblib.load('myVectorizer.pkl')
@@ -54,29 +223,42 @@ def predict_analysis(tweet:Tweet):
 @app.put("/twitter/")
 def tweet_analysis(url:Url):
     myURL = {url.URL}
-    myTweet,UserName = getTweetTextandUsername(next(iter(myURL)))
+    myTweet, UserName = get_tweet_text_and_username(next(iter(myURL)))
     Tweet_vector =  myVectorizer.transform({myTweet})
     prediction = model.predict(Tweet_vector)
-    return{'response':('looks positive to me! 😃' if prediction else 'looks negative to me!😕')
-    ,'tweet_text':f'{myTweet}','username':f'{UserName}'}
- 
+    return {
+        'response': 'looks positive to me! 😃' if prediction else 'looks negative to me!😕',
+        'tweet_text': f'{myTweet}',
+        'username': f'{UserName}'
+    }
 
 app.mount("/", StaticFiles(directory="staticfiles",html=True), name="static")
 @app.get("/")
 def read_root():
     return {"Hello":"World"}
 
-def getTweetTextandUsername(target_url:str):
-    # print("URL IS THIS :"+target_url)
-    # print("URL IS THIS :",str(type(target_url)))
+def get_tweet_text_and_username(target_url:str):
+def get_tweet_text_and_username(target_url:str):
     options = Options()
     options.add_argument('--headless')
-    driver=webdriver.Firefox( options=options)
-    driver.get(target_url)
-    time.sleep(5)
-    resp = driver.page_source
-    soup=BeautifulSoup(resp,'html.parser')
-    TweetText = soup.find("div",{"class":"css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-1inkyih r-16dba41 r-bnwqim r-135wba7"}).text
-    UserName = soup.find("div",{"class":"css-1rynq56 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-18u37iz r-1wvb978"}).text
-    driver.close()
-    return TweetText,UserName
+    
+    # Set timeout for WebDriver
+    driver = webdriver.Firefox(options=options)
+    driver.set_page_load_timeout(30)  # Adjust timeout as needed
+
+    try:
+        driver.get(target_url)
+        time.sleep(5)  # Wait for page to load (if needed)
+
+        resp = driver.page_source
+        soup = BeautifulSoup(resp, 'html.parser')
+        tweet_text = soup.find("div", {"class": "css-1rynq56 r-bcqeeo r-qvutc0 r-37j5jr r-1inkyih r-16dba41 r-bnwqim r-135wba7"}).text
+        tweet_text = tweet_text.replace('\n','')
+        UserName = soup.find("div", {"class": "css-1rynq56 r-dnmrzs r-1udh08x r-3s2u2q r-bcqeeo r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-16dba41 r-18u37iz r-1wvb978"}).text
+        return tweet_text, UserName
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        driver.quit()
+
+
